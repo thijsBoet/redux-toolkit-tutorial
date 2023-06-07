b9b33870d4de85d3fc7b1fac2483cac0cf5205a7
Matthijs Boet <m.boet@chello.nl>
2023-06-07 14:31:05 +0200

    learn basics redux store, state and reducer

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,1 +1,51 @@
-console.log('Hello World');
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const CAKE_ORDERED = 'CAKE_ORDERED';
+
+// ACTION
+// "order one cake"
+function orderCake() {
+    return {
+        type: CAKE_ORDERED,
+        quantity: 1
+    }
+}
+
+// INITIAL STATE
+const initialState = {
+	numOfCakes: 10,
+};
+
+// REDUCER
+// (previousState, action) => newState
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - action.quantity
+            }
+        default:
+            return state;
+    }
+};
+
+// STORE
+// Holds application state
+const store = createStore(reducer);
+console.log('Initial state: ', store.getState());
+
+// SUBSCRIBE
+// Called when state changes
+const unsubscribe = store.subscribe(() => console.log('Updated state: ', store.getState()));
+
+// DISPATCH
+// Dispatches an action
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+// UNSUBSCRIBE
+// Unsubscribe from store state changes
+unsubscribe();
